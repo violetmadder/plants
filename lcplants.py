e4961a51d5b86ed9e4e41e7ecd28b8ea98b9591f
violetmadder <35879374+violetmadder@users.noreply.github.com>
2018-02-14 18:39:42 -0800

    small change to readcsv() fixes EVERYTHING LIKE MAGIC
    thanks so much, Brian! Now I gotta study what you actually did.

--- lcplants.py
+++ lcplants.py
@@ -34,15 +34,74 @@ def taxons():
     return taxondefdict
 
 def dicts(t): return {k: dicts(t[k]) for k in t}
-# theoretically this converts the magical defaultdict back into a proper dict
+# theoretically this will convert the magical defaultdict back into a proper dict
 #pp.pprint(dicts(taxons()))
 
 
 def readcsv():
-    with open('USDAsearch.txt') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-    return reader
+      with open('USDAsearch.txt') as csvfile:
+          reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+          return [x for x in reader]
 
+
+def writejson():
+    with open('plantsdict.json', 'w') as outfile:
+        json.dump(plants, outfile, sort_keys=True, indent=4)
+
+def readjson():
+    with open ('plantsdict.json', 'r', encoding='utf8') as jsonfile:
+        return (json.load(jsonfile))
+
+def cleandict():
+    filteredItems = []
+    plantnum = 0
+    for item in readcsv():
+        plantnum = plantnum + 1
+        filteredItem = {}
+        for k, v in item.items(): #cleaning off keys with empty values
+            if v:
+                filteredItem[k] = v
+        if filteredItem:
+            filteredItems.append(filteredItem)
+    print ("this csv has " + str(plantnum) + " plants")
+    return filteredItems
+    # the above was based upon this: {k: [item for item in v if item] for k, v in readcsv())
+
+#def focusdict(): #trim down the big library into a smaller, more focused thing
+#    for item in cleandict():
+#        if item['Category'] == 'Lichen': #ignoring lichens
+#            continue
+#        else:
+#            focusdict.append(item)
+#    return focusdict
+
+def writekumu(): # repackaging the dict so it can be visualized in kumu (this is vestigial)
+    elements = []
+    for plant in cleandict():
+        sciname = plant['Scientific Name']
+        try:
+            plant['Common Name']
+            label = plant['Common Name']
+        except:
+            label =  sciname
+        elements.append({
+                'label': label,
+                '_id': plant['Symbol'],
+                'type' : 'species',
+                'description': '',
+                'tags': '',
+                'genus': plant['Genus'],
+                #'species': plant['Species'],
+                'scientific name': sciname,
+                #'common name': plant['Common Name'],
+                'zone' : '',
+                })
+    kumu = {'elements': elements, 'connections': []}
+    with open ('plantskumu.json', 'w') as outfile:
+        json.dump(kumu, outfile, sort_keys=True, indent=4)
+
+
+plants = cleandict()
 
 def writejson():
     with open('plantsdict.json', 'w') as outfile:
@@ -75,7 +134,7 @@ def cleandict():
 #            focusdict.append(item)
 #    return focusdict
 
-def writekumu(): # repackaging the dict so it can be visualized in kumu (this is vestigial)
+def writekumu(): # repackaging the dict so it can be visualized in kumu
     elements = []
     for plant in cleandict():
         sciname = plant['Scientific Name']
