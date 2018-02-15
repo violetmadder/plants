e349cffd645f6365b739a52ee1cf191b04fe068f
violetmadder <35879374+violetmadder@users.noreply.github.com>
2018-02-14 18:19:34 -0800

    took for loop out of readcsv

--- lcplants.py
+++ lcplants.py
@@ -28,22 +28,21 @@ def taxons():
             species = plant['Species']
 
         category = plant['Category']
- 
+
         taxondefdict[kingdom][order][pclass][family][genus] = species
- 
+
     return taxondefdict
 
-def dicts(t): return {k: dicts(t[k]) for k in t} 
+def dicts(t): return {k: dicts(t[k]) for k in t}
 # theoretically this converts the magical defaultdict back into a proper dict
 #pp.pprint(dicts(taxons()))
 
 
 def readcsv():
     with open('USDAsearch.txt') as csvfile:
-        for line in csvfile:
         reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-        rows = list(reader)
-    return rows
+    return reader
+
 
 def writejson():
     with open('plantsdict.json', 'w') as outfile:
@@ -58,7 +57,7 @@ def cleandict():
     plantnum = 0
     for item in readcsv():
         plantnum = plantnum + 1
-        filteredItem = {}    
+        filteredItem = {}
         for k, v in item.items(): #cleaning off empty values
             if v:
                 filteredItem[k] = v
@@ -76,7 +75,7 @@ def cleandict():
 #            focusdict.append(item)
 #    return focusdict
 
-def writekumu(): # repackaging the dict so it can be visualized in kumu
+def writekumu(): # repackaging the dict so it can be visualized in kumu (this is vestigial)
     elements = []
     for plant in cleandict():
         sciname = plant['Scientific Name']
@@ -91,7 +90,7 @@ def writekumu(): # repackaging the dict so it can be visualized in kumu
                 'type' : 'species',
                 'description': '',
                 'tags': '',
-                'genus': plant['Genus'], 
+                'genus': plant['Genus'],
                 #'species': plant['Species'],
                 'scientific name': sciname,
                 #'common name': plant['Common Name'],
@@ -100,5 +99,7 @@ def writekumu(): # repackaging the dict so it can be visualized in kumu
     kumu = {'elements': elements, 'connections': []}
     with open ('plantskumu.json', 'w') as outfile:
         json.dump(kumu, outfile, sort_keys=True, indent=4)
-        
-    
+
+
+plants = cleandict()
+writejson()
