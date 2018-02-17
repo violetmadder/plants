508513d1feaa9844833f95854495e02a9c1c0d0a
violetmadder <35879374+violetmadder@users.noreply.github.com>
2018-02-16 21:24:01 -0800

    made a readscv with io.open utf-16-le

--- lcplants.py
+++ lcplants.py
@@ -3,6 +3,7 @@ import json
 import pprint as pp
 from collections import defaultdict
 
+
 #taxonlist = ['Kingdom', 'Subkingdom', 'Superdivision', 'Division', 'Subdivision', 'Class', 'Subclass', 'Order', 'Family', 'Genus', 'Species', 'Subspecies', 'Variety', 'Subvariety', 'Cultivar', 'Forma']
 
 
@@ -32,12 +33,9 @@ def taxons():
         else:
             category = plant['Category']
 
-        taxondefdict[kingdom][order][pclass][family][genus] = species
+        taxondefdict[kingdom][order][pclass][family][genus][species]
 
     return taxondefdict
-
-
-
 
 def dicts(t): return {k: dicts(t[k]) for k in t}
 # theoretically this will convert the magical defaultdict back into a proper dict
@@ -46,11 +44,15 @@ def dicts(t): return {k: dicts(t[k]) for k in t}
 
 
 
-def readcsv():
+def readcsv(): #runs, but utf-16 characters come through this one as /u00 code
       with open('USDAsearch.txt', encoding = 'utf-8', errors = 'ignore') as csvfile:
           reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
           return [x for x in reader]
-
+      
+def ioreadcsv(): #trips "Error: field larger than field limit (131072)"
+      with io.open('USDAsearch.txt', 'r', encoding = 'utf-16-le') as csvfile:
+          reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+          return [x for x in reader]
 
 def writejson():
     with open('plantsdict.json', 'w') as outfile:
@@ -109,65 +111,26 @@ def writekumu(): # repackaging the dict so it can be visualized in kumu (this is
         json.dump(kumu, outfile, sort_keys=True, indent=4)
 
 
-
-
-def writejson():
-    with open('plantdict.json', 'w') as outfile:
-        json.dump(plants, outfile, sort_keys=True, indent=4)
-
-
-
-def readjson():
-    with open ('taxondict.json', 'r', encoding='utf8') as jsonfile:
-        return (json.load(jsonfile))
-
-def cleandict():
-    filteredItems = []
-    plantnum = 0
-    for item in readcsv():
-        plantnum = plantnum + 1
-        filteredItem = {}
-        for k, v in item.items(): #cleaning off empty values
-            if v:
-                filteredItem[k] = v
-        if filteredItem:
-            filteredItems.append(filteredItem)
-    print ("this csv has " + str(plantnum) + " plants")
-    return filteredItems
-    # the above was based upon this: {k: [item for item in v if item] for k, v in readcsv())
-
-#def focusdict(): #trim down the big library into a smaller, more focused thing
-#    for item in cleandict():
-#        if item['Category'] == 'Lichen': #ignoring lichens
-#            continue
-#        else:
-#            focusdict.append(item)
-#    return focusdict
-
-def writekumu(): # repackaging the dict so it can be visualized in kumu
+def writetaxonkumu(): # repackaging the dict so it can be visualized in kumu, vestigial
     elements = []
-    for plant in taxons():
-        sciname = plant['Scientific Name']
-        try:
-            plant['Common Name']
-            label = plant['Common Name']
-        except:
-            label =  sciname
+    for kingdom in taxons():
         elements.append({
-                'label': label,
-                '_id': plant['Symbol'],
-                'type' : 'species',
+                'label': kingdom,
+                'type' : 'kingdom',
                 'description': '',
                 'tags': '',
-                'genus': plant['Genus'],
-                #'species': plant['Species'],
-                'scientific name': sciname,
-                #'common name': plant['Common Name'],
-                'zone' : '',
                 })
+        for division in taxons[kingdom]:
+            elements.append({
+                    'label': division,
+                    'type': 'division',
+                    })
+            for pclass in taxons[kingdom][pclass]:
+                elements.append({
+                        'label': pclass,
+                        'type': 'class',
+                        })
     kumu = {'elements': elements, 'connections': []}
     with open ('plantskumu.json', 'w') as outfile:
         json.dump(kumu, outfile, sort_keys=True, indent=4)
 
-
-writekumu()
